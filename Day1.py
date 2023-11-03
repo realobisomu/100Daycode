d997694233f82b0c14fd19cc5211fcb6d358fb20
Obi Somuadina <obisomu@gmail.com>
2023-11-03 10:46:10 +0100

    100 day code day 1

---
+++ Day1.py
@@ -0,0 +1,46 @@
+# # Name counter
+
+# # name = input('Enter your name : ')
+# # name_count = len(name)
+# # print(f'Name Count: {name_count}')
+# # Get the user's name
+# name = input('Enter your name : ')
+# # Count the length of the name
+# name_count = len(name)
+# # Print the length of the name
+# print(f'Name Count: {name_count}')
+
+
+# # Test 1
+
+# # Get the user's input
+# a = input('Enter a : ')
+# # Get the user's input
+# b = input('Enter b:  ')
+# # Set c equal to a
+# c = a 
+# # Set a equal to b
+# a= b 
+# # Set b equal to c
+# b = c
+
+
+# # Print the values of a, b, and c
+# print(f'a = {a}')
+# print(f'b = {b}')
+
+# Day 1 - Project
+
+# print('Welcome to Band Name Picker')
+
+# cityInput = input('Enter city you grow up in :\n  ')
+# petName= input('Enter Pet name :\n ')
+
+# bandName = cityInput + " " + petName
+
+# print(f'Your band name is {bandName}')
+
+
+
+
+
